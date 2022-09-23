57b5e557413d90a3418a5951977261af43819789
Tina Colvin <thebigtoona@gmail.com>
2022-09-23 16:13:59 -0600

    redo ecs structure. add union for components. consider union for entity types as well.

--- src/ecs.zig
+++ src/ecs.zig
@@ -1,51 +1,71 @@
 const std = @import("std");
 
-const ComponentId: type = usize;
-const ComponentArray: type = std.Arraylist(*Component);
-const ComponentBitSet: type = std.Arraylist(bool);
+// ECS Notes:
+// entity interface
+// component interface 
+// game will need to hold an array of all the entities 
+// entities will need to hold an array of all their own components 
+// entities can have different components (player has an input comp but a misc prop might not have an input comp)
+// and compose components together for functionality
+// components are for describing the data and focus around the data (physics component, input component, sprite component, etc)
 
-const MAX_ENTITIES: usize = 32;
+const MAX_ENTITIES: u8 = 32;
 
-pub fn Component(T: type)
+// maybe use a union here instead of a fn type for holding type info on entities then I can easily switch on them the
+// same as I would components. this would create more consisitency.
+pub fn Entity(T: type) type
 {
-    return struct
+    return struct 
     {
-        entities: std.ArrayList(*Entity),
-    
+        /// the entity id. made this a u8 since we're maxing out at 32 anyway for now.
+        /// maybe use a larger integar in the future for this once I figure out how much 
+        /// i can even keep in the scene at once. 
+        id: u8,
+        
+        /// describes whether the entity is active or not. if !active then entity is destroyed via 
+        /// deinit and messages are sent to the msg system to tell eveything that needs this entity
+        /// hey I'm destroyed don't care about me anymore.
+        active: bool,
+        
+        //TODO: don't forget the answer to the problem you're having is a union, tina.
+        components: std.ArrayList(*Component),
+        
+        /// when the entity enters the scene
         pub fn init() void
         {}
-        
-        pub fn update() void 
+         
+        /// remove the entity and clean it up 
+        pub fn deinit() void
         {}
-        
-        pub fn draw() void 
+           
+        /// update the entity on tick
+        pub fn update() void
         {}
-    };
+    }
+}
+
+pub const PhysicsComponent = struct 
+{
+    position: tf_math.Vector2,
 };
 
-pub fn Entity(T: type) = 
+pub const RenderComponent = struct 
+{};
+
+pub const InputComponent = struct 
+{};
+
+pub const ComponentType = enum(u8)
 {
-    return struct
-    {
-        pub fn update()
-        {}
-        
-        pub fn draw()
-        {}
-        
-        pub fn destroy()
-        {}
-        
-        pub fn isActive()
-        {}
-        
-        pub fn hasComponent()
-        {}
-        
-        pub fn addComponent()
-        {}
-        
-        pub fn getComponent()
-        {}
-    };
+    Physics,
+    Input,
+    Render,    
 };
+
+pub const Component = union(ComponentType)
+{
+    Physics: PhysicsComponent,
+    Input: InputComponent,
+    Render: RenderComponent,
+};
+
